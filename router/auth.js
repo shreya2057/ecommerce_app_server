61bc9408f1b9733c396a7b816b586e361728425a
shreya <shreyashrestha2k57@gmail.com>
2024-03-26 15:52:00 +0545

    fix: vercel issue fixes

--- router/auth.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import auth_controller from "../controller/auth_controller.js";
-const router = Router();
-
-router.post("/signup", auth_controller.signup);
-router.post("/login", auth_controller.login);
-
-export default router;
