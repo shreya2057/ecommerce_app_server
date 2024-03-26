6bcd21f6257132487d875e2603616780cd07b6a1
shreya <shreyashrestha2k57@gmail.com>
2024-03-26 15:05:55 +0545

    fix: cors error fixes

--- index.js
+++ index.js
@@ -12,7 +12,24 @@ const port = 8000;
 // MongoDB Connection
 mongoDB_connection();
 
-app.use(cors());
+const allowedOrigins = ["http://localhost:3000"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 app.use(bodyParser.json());
 
 // app.use() -> The function that is used to add middleware function in
