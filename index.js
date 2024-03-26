c1a6dd8596f1f8ce52dd7c020d4a27896038d31b
shreya <shreyashrestha2k57@gmail.com>
2024-03-26 15:24:01 +0545

    fix: cors error fixes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use("/auth", auth_routes);
 app.use((_, res) => res.json({ message: "Page not found", status: 404 }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
