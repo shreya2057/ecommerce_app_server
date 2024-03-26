32079dacc060f8267c66b7f045c3bbdca4ded2ba
shreya <shreyashrestha2k57@gmail.com>
2024-03-26 15:00:36 +0545

    fix: cors fixes

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ app.use("/auth", auth_routes);
 // Handling routes that does not exist
 app.use((_, res) => res.json({ message: "Page not found", status: 404 }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // app.listen() -> The function that binds and listen to connections
 //                 on specified host and port.
 app.listen(port, () => console.log(`Server listening to the port ${port}`));
